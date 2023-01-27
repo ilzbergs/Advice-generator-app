cb78d9646cdb931f5c2a5e3d173bae24d8a15682
Toms Ilzbergs <ilzbergs@inbox.lv>
2023-01-27 22:27:42 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const dice = document.getElementById('dice');
+const advice = document.querySelector('.advice');
+const id = document.querySelector('.adviceId')
+
+window.onload = showAdvice;
+
+dice.addEventListener('click', ()=>{
+    showAdvice()
+})
+
+function showAdvice() {
+    fetch("https://api.adviceslip.com/advice")
+        .then(response => response.json())
+        .then((data) => data.slip)
+        .then((data) => {
+           id.innerHTML = data.id;
+            advice.innerHTML = data.advice;
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
